bcb59c44f9c8aa4bcc675d8fb087ea616272fdd9
Shin-Thant <shinthant10684@gmail.com>
2024-03-14 15:15:45 +0630

    remove socket port, update server port

--- src/socket/socket.gateway.ts
+++ src/socket/socket.gateway.ts
@@ -9,7 +9,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway(3000, {
+@WebSocketGateway({
   transport: ['websocket'],
   cors: {
     origin: '*',
